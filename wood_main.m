49ee7926dc2c95ae651a277f8df5bfb426a8fea0
MisajS <62326996+MisajS@users.noreply.github.com>
2021-07-06 10:27:08 +0530

    Update wood_main.m

--- wood_main.m
+++ wood_main.m
@@ -1,82 +1,174 @@
-addpath('C:\Users\acer\Desktop\Misaj\Project\Wood Dataset\Train\TRAIN 1_12','C:\Users\acer\Desktop\Misaj\Project\Wood Dataset\Test\TEST 1_12');
+clc
+clear all
 
-%Training Phase
-imagefiles = dir('C:\Users\acer\Desktop\Misaj\Project\Wood Dataset\Train\TRAIN 1_12\*.jpg');
+addpath('C:\Users\Lenovo\Desktop\Misaj\Project\Wood Dataset\Train','C:\Users\Lenovo\Desktop\Misaj\Project\Wood Dataset\Test');
+
+imagefiles = dir('C:\Users\Lenovo\Desktop\Misaj\Project\Wood Dataset\Train\*.jpg');
+
 nfiles = length(imagefiles);    % Number of files found
+
 for i=1:nfiles
-    	currentfilename = imagefiles(i).name;
+    currentfilename = imagefiles(i).name;
 	[indx1, indx2] = regexp(currentfilename,'\_[0-9]*\.');
-	label_values = currentfilename(indx1+1:indx2-1);
+	label_values = currentfilename(indx1+1:indx2-1)
 % 	disp(label_values)
 	currentimage = imread(currentfilename);
 	images{i} = currentimage;
+   
+    %images{i} = imresize(images{i},[512,512]);
+    %images{i} = imadjust(images{i},stretchlim(images{i}));
+    %rgb to gray
 	gray{i} = rgb2gray(images{i});
    
     
-    %GLCM feature extraction
-	glcms = graycomatrix(gray{i});
-
-    % Derive Statistics from GLCM
-	stats = graycoprops(glcms,'Contrast Correlation Energy Homogeneity');
-	Contrast = stats.Contrast;
-	Correlation = stats.Correlation;
-	Energy = stats.Energy;
-	Homogeneity = stats.Homogeneity;
+    %22 GLCM feature extraction
+	GLCM2 = graycomatrix(gray{i},'Offset',[2 0;0 2]);
+    stats = GLCM_Features1(GLCM2,0);
     
-	feature_value(i,1) = Contrast;
-	feature_value(i,2) = Correlation;
-	feature_value(i,3) = Energy;
-	feature_value(i,4) = Homogeneity;
-	feature_value(i,5) = str2double(label_values);
-        
-    	X_TRAIN(i,:) = feature_value(i,1:4);
-    	Y_TRAIN(i) = feature_value(i,5);
+    autocorrelation(1:2) = reshape((stats.autoc),2,[]);
+    contrast(1:2) = reshape((stats.contr),2,[]);
+    correlation_m(1:2) = reshape((stats.corrm),2,[]);
+    correlation(1:2) = reshape((stats.corrp),2,[]);
+    cluster_prominence(1:2) = reshape((stats.cprom),2,[]);
+    cluster_shade(1:2) = reshape((stats.cshad),2,[]);
+    dissimilarity(1:2) = reshape((stats.dissi),2,[]);
+    energy_m(1:2) = reshape((stats.energ),2,[]);
+    entropy(1:2) = reshape((stats.entro),2,[]);
+    homogeneity_m(1:2) = reshape((stats.homom),2,[]);
+    homogeneity(1:2) = reshape((stats.homop),2,[]);
+    max_probability(1:2) = reshape((stats.maxpr),2,[]);
+    variance(1:2) = reshape((stats.sosvh),2,[]);
+    sum_average(1:2) = reshape((stats.savgh),2,[]);
+    sum_variance(1:2) = reshape((stats.svarh),2,[]);     
+    sum_entropy(1:2) = reshape((stats.senth),2,[]);
+    difference_variance(1:2) = reshape((stats.dvarh),2,[]);
+    difference_entropy(1:2) = reshape((stats.denth),2,[]);
+    info_measure_correlation1(1:2) = reshape((stats.inf1h),2,[]);
+    info_measure_correlation2(1:2) = reshape((stats.inf2h),2,[]);
+    inv_diff_normalized(1:2) = reshape((stats.indnc),2,[]);
+    inv_diff_moment_normalized(1:2) = reshape((stats.idmnc),2,[]);
+    
+
+	feature_value(i,1) = (autocorrelation(1)+autocorrelation(2))/2;
+	feature_value(i,2) = (contrast(1)+contrast(2))/2;
+    feature_value(i,3) = (correlation_m(1)+correlation_m(2))/2;
+    feature_value(i,4) = (correlation(1)+correlation(2))/2;
+    feature_value(i,5) = (cluster_prominence(1)+cluster_prominence(2))/2;
+    feature_value(i,6) = (cluster_shade(1)+cluster_shade(2))/2;
+    feature_value(i,7) = (dissimilarity(1)+dissimilarity(2))/2;
+    feature_value(i,8) = (energy_m(1)+energy_m(2))/2;
+    feature_value(i,9) = (entropy(1)+entropy(2))/2;
+    feature_value(i,10) = (homogeneity_m(1)+homogeneity_m(2))/2;
+    feature_value(i,11) = (homogeneity(1)+homogeneity(2))/2;
+    feature_value(i,12) = (max_probability(1)+max_probability(2))/2;
+    feature_value(i,13) = (variance(1)+variance(2))/2;
+    feature_value(i,14) = (sum_average(1)+sum_average(2))/2;
+    feature_value(i,15) = (sum_variance(1)+sum_variance(2))/2;
+    feature_value(i,16) = (sum_entropy(1)+sum_entropy(2))/2;
+    feature_value(i,17) = (difference_variance(1)+difference_variance(2))/2;
+    feature_value(i,18) = (difference_entropy(1)+difference_entropy(2))/2;
+    feature_value(i,19) = (info_measure_correlation1(1)+info_measure_correlation1(2))/2;
+    feature_value(i,20) = (info_measure_correlation2(1)+info_measure_correlation2(2))/2;
+    feature_value(i,21) = (inv_diff_normalized(1)+inv_diff_normalized(2))/2;
+    feature_value(i,22) = (inv_diff_moment_normalized(1)+inv_diff_moment_normalized(2))/2;
+    feature_value(i,23) = str2double(label_values);
+    
+    
+    X_TRAIN(i,:) = feature_value(i,1:22);
+    Y_TRAIN(i) = feature_value(i,23);
 end
 
 
-%Testing Phase
-imagefiles1 = dir('C:\Users\acer\Desktop\Misaj\Project\Wood Dataset\Test\TEST 1_12\*.jpg');  
+imagefiles1 = dir('C:\Users\Lenovo\Desktop\Misaj\Project\Wood Dataset\Test\*.jpg');  
 nfiles1 = length(imagefiles1);    % Number of files found
 
 for k=1:nfiles1
     currentfilename1 = imagefiles1(k).name;
 	[indx1, indx2] = regexp(currentfilename1,'\_[0-9]*\.');
-	label_values1 = currentfilename1(indx1+1:indx2-1);
+	label_values1 = currentfilename1(indx1+1:indx2-1)
 % 	disp(label_values)
 	currentimage1 = imread(currentfilename1);
 	images{k} = currentimage1;
-   	gray1{k} = rgb2gray(images{k});
    
+    %images{i} = imresize(images{i},[512,512]);
+    %images{i} = imadjust(images{i},stretchlim(images{i}));
+    %rgb to gray
+	gray1{k} = rgb2gray(images{k});
+   
+    %22 GLCM feature extraction
+	GLCM2 = graycomatrix(gray1{k},'Offset',[2 0;0 2]);
+    stats1 = GLCM_Features1(GLCM2,0);
     
-    %GLCM feature extraction
-	glcms1 = graycomatrix(gray1{k});
+    autocorrelation(1:2) = reshape((stats1.autoc),2,[]);
+    contrast(1:2) = reshape((stats1.contr),2,[]);
+    correlation_m(1:2) = reshape((stats1.corrm),2,[]);
+    correlation(1:2) = reshape((stats1.corrp),2,[]);
+    cluster_prominence(1:2) = reshape((stats1.cprom),2,[]);
+    cluster_shade(1:2) = reshape((stats1.cshad),2,[]);
+    dissimilarity(1:2) = reshape((stats1.dissi),2,[]);
+    energy_m(1:2) = reshape((stats1.energ),2,[]);
+    entropy(1:2) = reshape((stats1.entro),2,[]);
+    homogeneity_m(1:2) = reshape((stats1.homom),2,[]);
+    homogeneity(1:2) = reshape((stats1.homop),2,[]);
+    max_probability(1:2) = reshape((stats1.maxpr),2,[]);
+    variance(1:2) = reshape((stats1.sosvh),2,[]);
+    sum_average(1:2) = reshape((stats1.savgh),2,[]);
+    sum_variance(1:2) = reshape((stats1.svarh),2,[]);     
+    sum_entropy(1:2) = reshape((stats1.senth),2,[]);
+    difference_variance(1:2) = reshape((stats1.dvarh),2,[]);
+    difference_entropy(1:2) = reshape((stats1.denth),2,[]);
+    info_measure_correlation1(1:2) = reshape((stats1.inf1h),2,[]);
+    info_measure_correlation2(1:2) = reshape((stats1.inf2h),2,[]);
+    inv_diff_normalized(1:2) = reshape((stats1.indnc),2,[]);
+    inv_diff_moment_normalized(1:2) = reshape((stats1.idmnc),2,[]);
+    
 
-    % Derive Statistics from GLCM
-	stats1 = graycoprops(glcms1,'Contrast Correlation Energy Homogeneity');
-	Contrast1 = stats1.Contrast;
-	Correlation1 = stats1.Correlation;
-	Energy1 = stats1.Energy;
-	Homogeneity1 = stats1.Homogeneity;
-    
-	feature_value1(k,1) = Contrast1;
-	feature_value1(k,2) = Correlation1;
-	feature_value1(k,3) = Energy1;
-	feature_value1(k,4) = Homogeneity1;
-	feature_value1(k,5) = str2double(label_values1);
-        
-    	X_TEST(k,:) = feature_value1(k,1:4);
-    	Y_TEST(k) = feature_value1(k,5);
+	feature_value1(k,1) = (autocorrelation(1)+autocorrelation(2))/2;
+	feature_value1(k,2) = (contrast(1)+contrast(2))/2;
+    feature_value1(k,3) = (correlation_m(1)+correlation_m(2))/2;
+    feature_value1(k,4) = (correlation(1)+correlation(2))/2;
+    feature_value1(k,5) = (cluster_prominence(1)+cluster_prominence(2))/2;
+    feature_value1(k,6) = (cluster_shade(1)+cluster_shade(2))/2;
+    feature_value1(k,7) = (dissimilarity(1)+dissimilarity(2))/2;
+    feature_value1(k,8) = (energy_m(1)+energy_m(2))/2;
+    feature_value1(k,9) = (entropy(1)+entropy(2))/2;
+    feature_value1(k,10) = (homogeneity_m(1)+homogeneity_m(2))/2;
+    feature_value1(k,11) = (homogeneity(1)+homogeneity(2))/2;
+    feature_value1(k,12) = (max_probability(1)+max_probability(2))/2;
+    feature_value1(k,13) = (variance(1)+variance(2))/2;
+    feature_value1(k,14) = (sum_average(1)+sum_average(2))/2;
+    feature_value1(k,15) = (sum_variance(1)+sum_variance(2))/2;
+    feature_value1(k,16) = (sum_entropy(1)+sum_entropy(2))/2;
+    feature_value1(k,17) = (difference_variance(1)+difference_variance(2))/2;
+    feature_value1(k,18) = (difference_entropy(1)+difference_entropy(2))/2;
+    feature_value1(k,19) = (info_measure_correlation1(1)+info_measure_correlation1(2))/2;
+    feature_value1(k,20) = (info_measure_correlation2(1)+info_measure_correlation2(2))/2;
+    feature_value1(k,21) = (inv_diff_normalized(1)+inv_diff_normalized(2))/2;
+    feature_value1(k,22) = (inv_diff_moment_normalized(1)+inv_diff_moment_normalized(2))/2;
+    feature_value1(k,23) = str2double(label_values1);
+
+    X_TEST(k,:) = feature_value1(k,1:22);
+    Y_TEST(k) = feature_value1(k,23);
 end
+ 
 
 KNN_model= fitcknn(X_TRAIN,Y_TRAIN);
 predicted_label = predict(KNN_model,X_TEST);
-% disp(KNN_model);
+disp(KNN_model);
 
-%Evaluation
-cm = confusionmat(Y_TEST,predicted_label);
-TP = cm(1,1);
-FN = cm(1,2);
-FP = cm(2,1);
-TN = cm(2,2);
-Accuracy = (TP+TN)/(TP+FP+FN+TN);
-disp(Accuracy);
+cm_22 = confusionmat(Y_TEST,predicted_label);
+
+TP_TN=0;
+FP_FN=0;
+for class_i=1:12
+    for class_j=1:12
+        if class_i==class_j
+            TP_TN = TP_TN+cm_22(class_i,class_j);
+        else
+            FP_FN = FP_FN+cm_22(class_i,class_j);
+        end
+    end
+end
+
+Accuracy = (TP_TN)/(TP_TN + FP_FN);
+disp(Accuracy)
